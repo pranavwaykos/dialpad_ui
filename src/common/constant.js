d2420a53101529a7e86cc576dcd0e84977d09794
Pranav Waykos <waykos.pranav@gmail.com>
2024-08-07 15:24:27 +0530

    Added new url

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,3 +1,3 @@
 export const API_KEY = 'fDsEEytAjRqkHvSAqs6ruyzdybRGwhLgEZrU8pkCLFmFArJehtcXdQZx6N8HkjZzgNT2WBF8DAcFt6UYbhCSVWR77rBA5rsEk6x7';
 export const GET_CONTACT_LIST_URL = "https://sandbox.dialpad.com/api/v2/contacts?cursor=&limit=10"
-export const CREATE_CONTACT_URL = "https://d1f8-115-113-153-34.ngrok-free.app/contacts" 
+export const CREATE_CONTACT_URL = "http://35.225.214.157:8080/contacts" 
